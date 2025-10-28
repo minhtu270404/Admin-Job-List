9ce7bdae166126dfe03a62d1f7f6e315c78e9ed2
minhtu270404 <minhtu270404>
2025-10-28 23:13:03 +0700

    5: Traits

---
+++ app/Http/Controllers/Admin/ProfileUpdateController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use App\Services\Notify;
+use App\Traits\FileUploadTrait;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class ProfileUpdateController extends Controller
+{
+   
+}
+
