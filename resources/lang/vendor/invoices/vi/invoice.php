42625bc0f8b63ef8a3f33e71846870b2866eb2d6
minhtu270404 <minhtu270404>
2025-10-28 22:50:38 +0700

    3: Crawl Admin Template

---
+++ resources/lang/vendor/invoices/vi/invoice.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Ngôn ngữ cho Hóa đơn
+    |--------------------------------------------------------------------------
+    */
+
+    'invoice'                => 'Hóa đơn',
+    'serial'                 => 'Số seri',
+    'date'                   => 'Ngày lập hóa đơn',
+    'seller'                 => 'Người bán',
+    'buyer'                  => 'Người mua',
+    'address'                => 'Địa chỉ',
+    'code'                   => 'Mã số',
+    'vat'                    => 'Mã số thuế (VAT)',
+    'phone'                  => 'Số điện thoại',
+    'description'            => 'Mô tả',
+    'units'                  => 'Đơn vị',
+    'quantity'               => 'Số lượng',
+    'price'                  => 'Đơn giá',
+    'discount'               => 'Giảm giá',
+    'tax'                    => 'Thuế',
+    'sub_total'              => 'Tạm tính',
+    'total_discount'         => 'Tổng giảm giá',
+    'taxable_amount'         => 'Giá trị tính thuế',
+    'total_taxes'            => 'Tổng thuế',
+    'tax_rate'               => 'Thuế suất',
+    'total_amount'           => 'Tổng cộng',
+    'pay_until'              => 'Vui lòng thanh toán trước ngày',
+    'amount_in_words'        => 'Số tiền bằng chữ',
+    'amount_in_words_format' => '%s %s và %s %s',
+    'notes'                  => 'Ghi chú',
+    'shipping'               => 'Phí vận chuyển',
+    'paid'                   => 'Đã thanh toán',
+    'due'                    => 'Còn nợ',
+];
