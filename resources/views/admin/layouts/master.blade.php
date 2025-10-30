7c817b699c02858244cb04aa60848589cdff4111
minhtu270404 <minhtu270404>
2025-10-30 13:08:43 +0700

    8: Update Layouts Admin

--- resources/views/admin/layouts/master.blade.php
+++ resources/views/admin/layouts/master.blade.php
@@ -37,7 +37,7 @@
             <footer class="main-footer">
                 <div class="footer-left">
                     Copyright &copy; {{ date('Y') }} <div class="bullet"></div> Design By <a
-                        href="#">MTU</a>
+                        href="www.websolutionus.com">websolutionus</a>
                 </div>
                 <div class="footer-right">
 
--- resources/views/admin/site-setting/index.blade.php
+++ resources/views/admin/site-setting/index.blade.php
@@ -3,38 +3,34 @@
 @section('contents')
 <section class="section">
     <div class="section-header">
-        <h1>Payment Gateway Settings</h1>
+        <h1>Site Settings</h1>
     </div>
 
     <div class="section-body">
         <div class="col-12">
             <div class="card">
                 <div class="card-header">
-                  <h4>All Gateway Settings</h4>
+                  <h4>All Settings</h4>
                 </div>
                 <div class="card-body">
                   <div class="row">
                     <div class="col-12 col-sm-12 col-md-2">
                       <ul class="nav nav-pills flex-column" id="myTab4" role="tablist">
                         <li class="nav-item">
-                          <a class="nav-link active" id="home-tab4" data-toggle="tab" href="#home4" role="tab" aria-controls="home" aria-selected="true">Paypal</a>
+                          <a class="nav-link active" id="home-tab4" data-toggle="tab" href="#home4" role="tab" aria-controls="home" aria-selected="true">Ganarel Setting</a>
                         </li>
                         <li class="nav-item">
-                          <a class="nav-link" id="profile-tab4" data-toggle="tab" href="#profile4" role="tab" aria-controls="profile" aria-selected="false">Stripe</a>
+                          <a class="nav-link" id="profile-tab4" data-toggle="tab" href="#profile4" role="tab" aria-controls="profile" aria-selected="false">Logo Setting</a>
                         </li>
-                        <li class="nav-item">
-                          <a class="nav-link" id="contact-tab4" data-toggle="tab" href="#contact4" role="tab" aria-controls="contact" aria-selected="false">Razorpay</a>
-                        </li>
+
                       </ul>
                     </div>
                     <div class="col-12 col-sm-12 col-md-10">
                       <div class="tab-content no-padding" id="myTab2Content">
 
-                        @include('admin.payment-setting.sections.paypal-section')
+                        @include('admin.site-setting.sections.general-section')
 
-                        @include('admin.payment-setting.sections.stripe-section')
-
-                        @include('admin.payment-setting.sections.razorpay-section')
+                        @include('admin.site-setting.sections.logo-section')
 
                       </div>
                     </div>
